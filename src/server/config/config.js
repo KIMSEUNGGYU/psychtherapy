15b7ed377b5a87eb469e709fafc7dbdfcffd204c
kimseunggyu <dev.seunggyu@gmail.com>
2020-07-18 23:41:43 +0900

    [feat] setting database default structure

---
+++ src/server/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USERNAME || "root",
+    password: process.env.DEV_DB_PASSWORD || "1234",
+    database: process.env.DEV_DB_DATABASE || "be-simple",
+    host: process.env.DEV_DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+    logging: false,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
