a60c92348f7f7537bdcdb9e005aa6f91943a6f44
leejunyoung93 <ab12b1125@gmail.com>
2020-07-27 11:58:58 +0900

    [feat] Add eslintrc

--- src/client/App.js
+++ src/client/App.js
@@ -4,26 +4,24 @@ import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
 import { store, history } from "client/store";
 import { LayerPopupContainer } from "client/libs/popup";
-import { PublicRoute, PrivateRoute } from "client/utils/routes";
+import { PublicRoute } from "client/utils/routes";
 import { PageHome, PageCounselors } from "client/pages";
 import { Header, Footer } from "client/components";
 
-const App = (props) => {
-    return (
-        <Provider store={store}>
-            <Router history={history}>
-                <Header {...props} />
-                <PublicRoute exact path="/" component={PageHome} />
-                <PublicRoute
-                    exact
-                    path="/counselors"
-                    component={PageCounselors}
-                />
-                <Footer />
-                <LayerPopupContainer {...props} />
-            </Router>
-        </Provider>
-    );
-};
+const App = (props) => (
+    <Provider store={store}>
+        <Router history={history}>
+            <Header {...props} />
+            <PublicRoute exact path="/" component={PageHome} />
+            <PublicRoute
+                exact
+                path="/counselors"
+                component={PageCounselors}
+            />
+            <Footer />
+            <LayerPopupContainer {...props} />
+        </Router>
+    </Provider>
+);
 
 export default App;
