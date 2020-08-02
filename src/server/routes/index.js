5b5f86c13420017135a764a99569a3621cd7efdb
JeongHM <kjhm0607@gmail.com>
2020-08-02 19:17:38 +0900

    [refactor] add schedule api route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -7,6 +7,7 @@ const partner = require("./partner");
 const admin = require("./admin");
 const keyword = require("./keyword");
 const point = require("./point");
+const schedule = require("./schedule");
 
 router.use(`${BASEURL}/`, middleware.cors);
 router.use(`${BASEURL}/user`, user);
@@ -14,5 +15,6 @@ router.use(`${BASEURL}/partner`, partner);
 router.use(`${BASEURL}/admin`, admin);
 router.use(`${BASEURL}/keyword`, keyword);
 router.use(`${BASEURL}/point`, point);
+router.use(`${BASEURL}/schedule`, schedule);
 
 module.exports = router;
