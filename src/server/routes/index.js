91023f76d97f5ed3ae7bccc982c8d854b5e9496f
JeongHM <kjhm0607@gmail.com>
2020-08-02 17:07:20 +0900

    [feat] add point api route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -6,11 +6,13 @@ const user = require("./user");
 const partner = require("./partner");
 const admin = require("./admin");
 const keyword = require("./keyword");
+const point = require("./point");
 
 router.use(`${BASEURL}/`, middleware.cors);
 router.use(`${BASEURL}/user`, user);
 router.use(`${BASEURL}/partner`, partner);
 router.use(`${BASEURL}/admin`, admin);
 router.use(`${BASEURL}/keyword`, keyword);
+router.use(`${BASEURL}/point`, point);
 
 module.exports = router;
