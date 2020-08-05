e3af9779c069870af092b2e29ab2e8c2f5dd820c
JeongHM <kjhm0607@gmail.com>
2020-08-05 23:09:16 +0900

    [refactor] add try catch in keyword controller
    - add when occur exception return 400 error

--- src/server/routes/keyword/controller.js
+++ src/server/routes/keyword/controller.js
@@ -3,16 +3,14 @@ const service = require("./service");
 exports.keyword = async (req, res, next) => {
   const keywords = await service.getKeywords();
 
-  keywords.length
-    ? res.status(200).json({
-        mesage: "KEYWORD LIST",
-        result: { keyword: keywords },
-      })
-    : res
-        .status(200)
-        .json({ mesage: "KEYWORD LIST FAIL - IT DOSEN'T EXIST", result: {} });
-
-  // 있어도 없어도 200
-  // 없을 경우 204 괜찮을 듯!! <- 수신을 했지만 컨텐츠 없음
-  // 400(api), 401,
+  try {
+    keywords.length
+      ? res.status(200).json({
+          mesage: "Success",
+          result: { keyword: keywords },
+        })
+      : res.status(204).json({ mesage: "Empty Object", result: {} });
+  } catch (err) {
+    res.status(400).json({ mesage: "Bad Request", result: {} });
+  }
 };
