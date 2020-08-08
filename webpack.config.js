084b6d21f8e3bc517b874ae9d03d8b8b9c21c2dd
kimseunggyu <dev.seunggyu@gmail.com>
2020-08-08 17:08:36 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,56 +6,63 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const outputDirectory = "dist";
 
 module.exports = {
-    entry: ["babel-polyfill", "./src/client/index.js"],
-    output: {
-        path: path.join(__dirname, outputDirectory),
-        filename: "bundle.[hash].js",
-        publicPath: '/'
+  entry: ["babel-polyfill", "./src/client/index.js"],
+  //   output: {
+  //     filename: "bundle.[hash].js",
+  //     // publicPath: "./",
+  //     publicPath: "/",
+  //     path: path.join(__dirname, outputDirectory),
+  //   },
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: "[name].[hash].js",
+    publicPath: "./",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                loader: "url-loader?limit=100000"
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                loader: "file-loader?name=dist/src/client/images/[name].[ext]"
-            }
-        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000",
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: "file-loader?name=/src/client/images/[name].[ext]",
+        // loader: "file-loader?name=dist/src/client/images/[name].[ext]",
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    modules: [path.join(__dirname, "src"), "node_modules"],
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    open: true,
+    hot: true,
+    inline: true,
+    proxy: {
+      "/api": "http://localhost:8080",
     },
-    resolve: {
-        extensions: ["*", ".js", ".jsx"],
-        modules: [path.join(__dirname, "src"), "node_modules"]
-    },
-    devServer: {
-        historyApiFallback: true,
-        port: 3000,
-        open: true,
-        hot: true,
-        inline: true,
-        proxy: {
-            "/api": "http://localhost:8080"
-        }
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new CleanWebpackPlugin([outputDirectory]),
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-            favicon: "./public/favicon.ico"
-        })
-    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+    }),
+  ],
 };
