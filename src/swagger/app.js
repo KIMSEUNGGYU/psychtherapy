e6d7d4bf4bd116088c273e1666be83d9d5de773d
kimseunggyu <dev.seunggyu@gmail.com>
2020-08-09 12:49:29 +0900

    [refactor] update swagger

--- src/swagger/app.js
+++ src/swagger/app.js
@@ -1,29 +1,10 @@
 const express = require("express");
-const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./docs/be-simple.json");
 
 const app = express();
 
-const swaggerDefinition = {
-  components: {},
-  openapi: "3.0.1",
-  info: {
-    title: "Swagger API V1",
-    version: "0.1.0",
-    description: "EveryThing About V1 SWAGGER TUTORIAL",
-  },
-  //   host: "blockscan.shop", // the host or url of the app
-  basePath: "/api/v1",
-};
-
-const options = {
-  swaggerDefinition,
-  apis: ["./docs/*.yaml"],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
-
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const server = app.listen(process.env.PORT || 3030, () => {
   console.log(`'Listening on port '${server.address().port}`);
