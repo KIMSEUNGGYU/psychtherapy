6135d33996026dca8963d52b3e99f1af6c96a1e0
kimseunggyu <dev.seunggyu@gmail.com>
2020-08-09 14:02:31 +0900

    [feat] github acition develop

--- src/server/routes/keyword/view.js
+++ src/server/routes/keyword/view.js
@@ -1,3 +1,6 @@
-exports.keywords = keywords => ({ message: "Success", result: { keywords } });
+exports.keywords = keywords => ({
+  message: "Success github action test",
+  result: { keywords },
+});
 exports.empty = () => ({ message: "Empty Object", result: {} });
 exports.badRequest = () => ({ mesage: "Bad Request", result: {} });
