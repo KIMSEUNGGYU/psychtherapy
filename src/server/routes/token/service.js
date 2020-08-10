aa293dd6efc0f3d948b6f8767e80d350b2de649b
kimseunggyu <dev.seunggyu@gmail.com>
2020-08-10 22:57:57 +0900

    [refactor] remove not needed code

--- src/server/routes/token/service.js
+++ src/server/routes/token/service.js
@@ -5,13 +5,6 @@ exports.verifyRefreshToken = async refreshToken =>
   await jwt.verify(refreshToken, false);
 
 exports.updateAccessToken = async (payload, refreshToken) => {
-  //   const { userId } = jwt.verify(refreshToken, false);
-
-  //   if (!userId) return false; // userId 가 없으면 verify 실패
-
-  //   const payload = {
-  //     userId,
-  //   };
   const accessToken = await jwt.generateAccessToken(payload);
 
   const success = await models.tokens.updateAccessToken(
