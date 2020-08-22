b104fa5c930867b58a3826fdc8b20f341b12baa4
JeongHM <kjhm0607@gmail.com>
2020-08-23 00:07:18 +0900

    [feat] upload s3 service

--- src/server/routes/admin/service.js
+++ src/server/routes/admin/service.js
@@ -1,5 +1,6 @@
 const sequelize = require("../../db/models").sequelize;
 const models = require("../../db/models");
+const AWS = require("aws-sdk");
 
 exports.getUsers = async (page, size) => {
   const limit = size;
@@ -23,7 +24,33 @@ exports.getPartners = async (page, size, evaluate) => {
   return partners;
 };
 
+exports.uploadImageFile = (partnerId, image) => {
+  let buf = new Buffer(image.replace(/^data:image\/\w+;base64,/, ""), "base64");
+  let bucketName = "be-simple";
+  let phase = "dev";
+  let fileName = "test.jpg";
+  param = {
+    Bucket: bucketName,
+    Key: `${phase}/${fileName}`,
+    ContentEncoding: "base64",
+    Body: buf,
+    ACL: "public-read",
+    ContentType: "image/jpeg",
+  };
+  s3.upload(param, (err, data) => {
+    if (err) console.log(err);
+    return data.Location;
+  });
+};
+
 exports.updatePartnerDetail = async (partnerId, partner) => {
+  console.log("partner.image: ", partner.image);
+  if (partner.image) {
+    let image_url = await this.uploadImageFile(paratnerId, partner.image);
+    console.log("image_url: ", image_url);
+    partner.image = image_url;
+  }
+  console.log("partner.image: ", partner.image);
   const result = await models.partnerDetails.updatePartner(partnerId, partner);
   return result[0]; // query 결과가 배열로 반환됨.
 };
