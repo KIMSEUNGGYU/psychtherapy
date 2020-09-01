cb3aaa2880fe862c197838a18b4a19d3bb367702
JeongHM <kjhm0607@gmail.com>
2020-09-01 21:14:58 +0900

    [fix] /chat API Response Object duplicate Key Name, Empty Object Response

--- src/server/routes/chat/service.js
+++ src/server/routes/chat/service.js
@@ -5,12 +5,14 @@ exports.getContentByRoomId = async roomId => {
   try {
     const contents = [];
     const content = await models.messages.getContentByRoomId(roomId);
+
+    if (content === null) return null;
+
     const jsonContent = JSON.parse(content.content);
 
     for (index of Object.keys(jsonContent)) {
       contents.push(jsonContent[index]);
     }
-
     return contents;
   } catch (e) {
     console.log(e);
